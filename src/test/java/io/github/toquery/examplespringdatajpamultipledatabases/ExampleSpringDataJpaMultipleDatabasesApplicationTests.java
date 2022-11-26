06fb6c54db8fcd1dcc74d4e2d9054a7ff06e1a5e
ToQuery <toquery@qq.com>
2022-11-26 10:02:28 +0800

    init

---
+++ src/test/java/io/github/toquery/examplespringdatajpamultipledatabases/ExampleSpringDataJpaMultipleDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringdatajpamultipledatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringDataJpaMultipleDatabasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
